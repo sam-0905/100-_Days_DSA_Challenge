6f5c76bc0e55023c46faed48f0095e6c12a4c2f2
Allan sam <allansam0905@gmail.com>
2026-08-31 22:49:56 +0530

    Built i method added using MAx

---
+++ Day-1/Python/Max.py
@@ -0,0 +1,18 @@
+#  Brute force solution 
+
+arr = [1,2,3,4,5,6]
+
+def findMax(num) :
+    max = 0
+    for i in arr:
+        if(i > max):
+            max = i
+    return max 
+ 
+result = findMax(arr)
+
+print(result)
+
+
+# Built in method
+find_max = lambda num: max(num)
