5e51d22357cd997d107d834aba0b19c7042a11da
Allan sam <allansam0905@gmail.com>
2026-09-01 18:41:20 +0530

    Brute for method is added for Max num

--- Largest_day_1/Max.jsx
+++ Largest_day_1/Max.jsx
@@ -1,7 +1,31 @@
-// Brute force solution 
-
-// Time and Space complexity is O(n)
 let arr = [1,2,3,4,5,6]
+
+// Brute force solution 
+/**
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ */ 
+
+for(let i=0; i<arr.length; i++){
+    isLargest = true
+
+    for(let j=0; j<arr.length; j++){
+        if(arr[j] > arr[i]){
+            isLargest = false
+            break
+        }
+    }
+    if(isLargest){
+        return arr[i]
+    }
+}
+
+
+// Track Maximum 🚀
+/** 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */ 
 
 function findMaxNum(num){
     let max = -Infinity
@@ -15,3 +39,5 @@ function findMaxNum(num){
 }
 
 findMaxNum(arr)
+
+
