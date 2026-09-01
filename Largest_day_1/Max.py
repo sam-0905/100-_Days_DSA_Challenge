c454919a41a2d0b0f3aef3f52ac2cb6b6cedc6f6
Allan sam <allansam0905@gmail.com>
2026-09-01 18:59:30 +0530

    brute force method added

--- Largest_day_1/Max.py
+++ Largest_day_1/Max.py
@@ -1,6 +1,26 @@
-#  Brute force solution 
 
 arr = [1,2,3,4,5,6]
+
+#  Brute force solution 
+
+
+def find_largest(arr):
+    for i in range(len(arr)):
+        is_largest = True
+
+        for j in range(len(arr)):
+            if arr[j] > arr[i]:
+                is_largest = False
+                break
+
+        if is_largest:
+            return arr[i]
+
+    result = find_largest(arr)
+
+    print(result)
+
+#  Track Maximum 🚀
 
 def findMax(num) :
     max = 0
