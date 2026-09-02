31ad0efa169a7389c759eb71475e6997c17fa91a
Allan sam <allansam0905@gmail.com>
2026-09-02 22:45:36 +0530

    Added a optimized approach for reverse

--- Reverse_day_3/reverse.py
+++ Reverse_day_3/reverse.py
@@ -9,3 +9,16 @@ result = reverse_string(arr)
 print(result)  
 
 
+# Optimized solution
+
+def reverse_string_optimized(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
+
+result = reverse_string_optimized(arr)
+print(result)  
