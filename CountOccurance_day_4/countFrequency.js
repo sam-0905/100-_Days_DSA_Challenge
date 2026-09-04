81b8550c444326b94169e226b549102f0dc7ac97
Allan sam <allansam0905@gmail.com>
2026-09-04 10:29:06 +0530

    .

--- CountOccurance_day_4/countFrequency.js
+++ CountOccurance_day_4/countFrequency.js
@@ -3,16 +3,18 @@
 // Time complexity : O(n^2)
 // Space complexity : O(n) 
 
+arr = [1,1,2,3,4,5,6,4]
+
 function countFrequency(arr){
     let seen = [];
     let result = {}
-    let count = 0
 
     for(let i=0; i<arr.length; i++){
         if(seen.includes(arr[i])){
             continue;
         }
-    
+    let count = 0
+
     for(let j =0; i,arr.length; j++){
         if(arr[i] === arr[j]){
             count ++
