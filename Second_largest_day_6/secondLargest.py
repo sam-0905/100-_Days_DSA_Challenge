44476719effba7fc61a42a528a17a78d8858fb2e
Allan sam <allansam0905@gmail.com>
2026-09-08 21:39:48 +0530

    Optimized approach was added for second largest

--- Second_largest_day_6/secondLargest.py
+++ Second_largest_day_6/secondLargest.py
@@ -20,3 +20,23 @@ print(result)  # Output: 4
 
 
 
+
+# Optimized approach
+# time complexity: O(n)
+# space complexity: O(1)
+
+def  second_largest_opt(arr):
+    if len(arr) <2:
+        return None
+    largest = second_largest = float('-inf')
+    second_largest = float('-inf')
+    for i in range(len(arr)):
+        if arr[i] >largest:
+            second_largest = largest
+            largest = arr[i]
+        elif arr[i] > second_largest and arr[i] != largest:
+            second_largest = arr[i]
+    return second_largest
+
+result = second_largest_opt([1, 2, 3, 4, 5])
+print(result)  # Output: 4
