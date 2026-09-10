82cc406bcd06af9ac14be798295914dc9479e7d9
Allan sam <allansam0905@gmail.com>
2026-09-10 17:42:57 +0530

    File - created duplicate.jsx and added a Brute force solution

---
+++ Dupilcate_day_7/duplicate.jsx
@@ -0,0 +1,13 @@
+// Brute force method
+
+
+function duplicate(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[i] === arr[j]){
+                return arr[i]
+            }
+    }
+}
+return null;
+}
