2dcbd636c220dd3fb397fe0a8eb4beaed4662751
Allan sam <allansam0905@gmail.com>
2026-09-13 21:41:04 +0530

    Added optimized approach for  palindrome

--- Palindrom/palindrome.py
+++ Palindrom/palindrome.py
@@ -16,4 +16,17 @@ result = isPalindrome(s)
 
 
 
+# Optimized approach
 
+def isPalindromeFun(s):
+    left =0
+    right = (len(s)) -1
+
+    while(left<right):
+        if(s[left] != s[right]):
+            return False
+
+        left +=1
+        right -=1
+        
+    return True
