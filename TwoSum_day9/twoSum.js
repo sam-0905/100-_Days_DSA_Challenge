bf99ce0ef6d1f88c7bc594c3ddabd1d38c063398
Allan sam <allansam0905@gmail.com>
2026-09-17 21:33:59 +0530

    File - created twoSum.js and added a Brute force solution

---
+++ TwoSum_day9/twoSum.js
@@ -0,0 +1,17 @@
+// Brute force method 
+// Time complexity O(n^2)
+// space complexity O(n)
+
+function twoSum(arr, target){
+
+    for(let i=0; i<arr.length; i++){
+        for(let j = i+1; i<arr.length; i++){
+            if(arr[i] + arr[j] === target){
+                return [arr[i] , arr[j]]
+            }
+        }
+    }
+
+    return []
+
+}
