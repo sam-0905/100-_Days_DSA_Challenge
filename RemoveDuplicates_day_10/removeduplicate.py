ffefb87f4118cc73d5b950337c76f189edec1485
Allan sam <allansam0905@gmail.com>
2026-09-18 19:26:03 +0530

    Added optimized approach for  removeDuplicatesArr

--- RemoveDuplicates_day_10/removeduplicate.py
+++ RemoveDuplicates_day_10/removeduplicate.py
@@ -13,6 +13,32 @@ def checkDuplicate(arr):
     return result
 
 
+# Optimized way
+# Time: O(n) 
+# Space: O(1)
+
+
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+
+    i = 0
+
+    for j in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            i += 1
+            arr[i] = arr[j]
+
+    return i + 1
+
+arr = [1, 1, 2, 2, 3, 4, 4]
+
+k = remove_duplicates(arr)
+
+print(k)          # 4
+print(arr[:k])    # [1, 2, 3, 4]
+
+
 # using set
 
 # Time: O(n)
