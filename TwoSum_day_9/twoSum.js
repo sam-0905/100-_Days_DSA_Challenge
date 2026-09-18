db2ca65f1e41690714067d166fb3592997986cbd
Allan sam <allansam0905@gmail.com>
2026-09-18 18:58:30 +0530

    fix - in j loop instead of j it is i

--- TwoSum_day_9/twoSum.js
+++ TwoSum_day_9/twoSum.js
@@ -5,7 +5,7 @@
 function twoSum(arr, target){
 
     for(let i=0; i<arr.length; i++){
-        for(let j = i+1; i<arr.length; i++){
+        for(let j = i+1; j<arr.length; j++){
             if(arr[i] + arr[j] === target){
                 return [arr[i] , arr[j]]
             }
