740af2648b9ac53b2a03786a2a47d6408edf276b
Allan sam <allansam0905@gmail.com>
2026-09-21 19:52:41 +0530

    Added swap  approach for  movesZerosToEnd

--- MoveAllZerors_day_11/zeros.py
+++ MoveAllZerors_day_11/zeros.py
@@ -38,3 +38,14 @@ def movesZerosToEnd(arr):
 
        return arr
  
+# Swap
+
+def move_zeros(arr):
+    insert_pos = 0
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[insert_pos], arr[i] = arr[i], arr[insert_pos]
+            insert_pos += 1
+
+    return arr
