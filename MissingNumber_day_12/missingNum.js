580b83fedb02f7dfecf4e2ca695d1cf14f8cc8ae
Allan sam <allansam0905@gmail.com>
2026-09-24 19:08:36 +0530

    File - created MissingNum.js and added a Brute force solution

---
+++ MissingNumber_day_12/missingNum.js
@@ -0,0 +1,14 @@
+// Brute force method
+
+
+function checkTheMissingNum(arr){
+
+    const n = arr.length
+
+    for(let i=0; i<=n; i++){
+        if(!arr.includes(num)){
+            return numm
+        }
+    }
+    return -1
+}
