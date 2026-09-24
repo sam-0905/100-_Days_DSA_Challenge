d8a8ce6f9bb5a6ba0e216558afa839d577691d7a
Allan sam <allansam0905@gmail.com>
2026-09-24 20:01:22 +0530

    Added optimized  approach for  findMissingNumber

--- MissingNumber_day_12/missingNum.js
+++ MissingNumber_day_12/missingNum.js
@@ -12,3 +12,19 @@ function checkTheMissingNum(arr){
     }
     return -1
 }
+
+
+// Optimized method
+
+function findMissingNumber(arr){
+        const n = arr.length
+
+        const expectedSum = n *(n +1 ) / 2
+
+        let actualNum = 0
+
+        for(num of arr){
+         actualNum += num
+        }
+        return expectedSum - actualNum
+}
--- MissingNumber_day_12/missingNum.py
+++ MissingNumber_day_12/missingNum.py
@@ -10,3 +10,17 @@ def checkTheMissingNum(arr):
             return i
     return -1
 
+# Optimized method
+
+def findMissingNumber(arr):
+
+    n = len(arr)
+
+    expectedNum = n * (n+1) // 2
+
+    actualNum = 0 
+
+    for num in arr:
+        actualNum += 1
+
+    return expectedNum - actualNum  
